0498eac75b3bcbab5f9ad754efe36790b90900f6
Kishore-Balasubramani <kishorebalasubramani.2006@gmail.com>
2026-02-23 10:36:13 +0530

    Fix before branch switch

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -1,38 +1,27 @@
 import java.util.*;
 public class PallindromeCheckerApp {
     private static final String App_version = "1.0";
-    public static void main(String[] args){
+
+    public static void main(String[] args) {
         System.out.println("welcome to palindrome checker management system");
-        System.out.println("Version : " +App_version);
+        System.out.println("Version : " + App_version);
         System.out.println("System initialized successfully.");
 
-//        String str = new String("madam");
-//        String isPalindrome = new String("True");
-//        int j= str.length();
-//        for(int i=0;i<str.length()/2;i++){
-//            j=j-1;
-//            if(str.charAt(i)!= str.charAt(j)){
-//                isPalindrome = "False";
-//            }
-//        }
-//        if(isPalindrome.equals("True")){
-//            System.out.println("is it a palindrome? : True");
-//        }
-//        else{
-//            System.out.println("is it a palindrome? : False");
-//        }
-
-        String rev = new String("");
         String str = new String("madam");
-
-        for(int i= str.length()-1;i>=0;i--){
-            rev+= str.charAt(i);
-        }
-        if(rev.equals(str)){
-            System.out.println("is it a palindrome? : True");
-        }
-        else{
+        String isPalindrome = new String("True");
+       int j= str.length();
+        for(int i=0;i<str.length()/2;i++){
+            j=j-1;
+           if(str.charAt(i)!= str.charAt(j)){
+              isPalindrome = "False";
+            }
+     }
+    if(isPalindrome.equals("True")){
+         System.out.println("is it a palindrome? : True");
+     }
+      else{
             System.out.println("is it a palindrome? : False");
         }
-    }
+   }
+}
 
