544f444570f4deba851661af5f63ae210b3047a0
Kishore-Balasubramani <kishorebalasubramani.2006@gmail.com>
2026-02-23 11:04:22 +0530

    UC4:Character Array Based Palindrome Check

--- src/PallindromeCheckerApp.java
+++ src/PallindromeCheckerApp.java
@@ -21,10 +21,38 @@ public class PallindromeCheckerApp {
         }
 
 
+        char[] chars= str.toCharArray();
+        int start=0;
+        int end= chars.length -1;
+        boolean isPalindrome= true;
+        while(start<end){
+            if(chars[start]!=chars[end]){
+                isPalindrome=false;
+                break;
+            }
+            start++; end--;
+        }
+
+        if(isPalindrome){
+            System.out.println("Is it a palindrome?: True");
+        }else{
+            System.out.println("Is it a palindrome?: False");
         }
 
 
+    }
 
-   }
+
+
+
+
+}
+
+
+
+
+
+
+
 
 
